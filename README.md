93f5ff5b13ec96834703af3fc939f62c63ad2ced
Haydar QS <haqy.x2@gmail.com>
2025-09-01 10:05:02 +0900

    menambahkan input untuk semua value dalam map

--- README.md
+++ README.md
@@ -4,7 +4,11 @@ NPM 07352211025
 
 --DESKRIPSI--
 Program ini menghitung jumlah gaji karyawan berdasarkan statusnya karena akan dipotong pajak.
-Yang dimana pada program saya, status tetapnya adalah TRUE maka gajinya akan dipotong pajak 10%.
+Yang dimana pada program saya, jika input status tetapnya adalah true, maka gajinya akan dipotong pajak 10%.
+namun jika input status tetapnya adalah false, maka gajinya akan dipotong pajak 5%.
 
 --INSTRUKSI--
-Cara menjalankan programnya hanya dengan di run saja, tidak ada instruksi untuk menambahkan input apapun.
+Cara menjalankan programnya yaitu:
+1. running di terminal agar bisa dilakukan proses input
+2. input data karyawan seperti nama, jam kerja, upah per jam dan status tetap (true/false).
+3. program akan menampilkan slip gaji yang terdiri dari nama karyawan, gaji kotor, pajak dan gaji bersih.
--- src/tugas.dart
+++ src/tugas.dart
@@ -1,24 +1,29 @@
+import 'dart:io';
+
 void main(List<String> args) {
+  print("Masukkan data karyawan: ");
+  stdout.write("Nama: ");
+  String? nama = stdin.readLineSync();
+  stdout.write("Jam kerja (seminggu): ");
+  int? jamKerja = int.parse(stdin.readLineSync()!);
+  stdout.write("Upah per jam: ");
+  double? upahPerJam = double.parse(stdin.readLineSync()!);
+  stdout.write("Status Tetap (true/false): ");
+  bool? statusTetap = stdin.readLineSync() == "true";
+
   Map<String, dynamic> karyawan = {
-    "nama": "Haydar",
-    "jamKerja": 35,
-    "upahPerJam": 100000.0,
-    "statusTetap": true
+    "nama": nama,
+    "jamKerja": jamKerja,
+    "upahPerJam": upahPerJam,
+    "statusTetap": statusTetap
   };
 
-  String nama = karyawan["nama"];
-  int jamKerja = karyawan["jamKerja"];
-  double upahPerJam = karyawan["upahPerJam"];
-  bool statusTetap = karyawan["statusTetap"];
-
-  double gajiKotor = jamKerja * upahPerJam;
-  double pajak = statusTetap ? gajiKotor * 0.1 : 0.05 * gajiKotor;
+  double gajiKotor = karyawan ["jamKerja"] * karyawan["upahPerJam"]; 
+  double pajak = karyawan["statusTetap"] ? gajiKotor * 0.1 : 0.05 * gajiKotor;
   double gajiBersih = gajiKotor - pajak;
 
-  print("Nama: $nama");
-  print("Jam Kerja: $jamKerja jam");
-  print("Upah Per Jam: Rp$upahPerJam");
-  print("Status Tetap: $statusTetap");
+  print("\n--Slip Gaji--");
+  print("Nama Karyawan: $nama");
   print("Gaji Kotor: Rp$gajiKotor");
   print("Pajak: Rp$pajak");
   print("Gaji Bersih: Rp$gajiBersih");
